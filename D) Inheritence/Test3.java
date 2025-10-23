aedc46c65853c698e011a1db4d155dd54edab537
Nithin KV <gowdanithin232@gmail.com>
2025-10-23 07:55:35 +0530

    Add files via upload

---
+++ D) Inheritence/Test3.java
@@ -0,0 +1,14 @@
+class A{
+    int y=20;
+}
+public class Test3 extends A {
+    int x=10;
+    public static void main(String[] args) {
+        Test3 T=new Test3();
+        System.out.println(T.y);
+        System.out.println(T.x);
+        }
+}
+
+
+//SINGLE-LEVEL-INHERITENCE
