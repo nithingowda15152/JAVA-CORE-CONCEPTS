62275c3b87c01034c46c7d2d31157d1d87a5d2e4
Nithin KV <gowdanithin232@gmail.com>
2025-10-25 08:14:23 +0530

    Add files via upload

---
+++ F) FinalKeyword(Modifier)/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String[] args) {
+        final int i=10;
+        final int j;
+        System.out.println(i);
+        // i=20;
+        // i++;
+        // i+=4;
+        j=i+4;
+    }
+}
