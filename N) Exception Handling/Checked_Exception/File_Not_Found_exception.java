8cb6b0e635295ce8b745ee77382cbe0314e67b04
Nithin KV <gowdanithin232@gmail.com>
2026-03-07 20:04:27 +0530

    Implement basic file handling with exception catch

--- N) Exception Handling/Checked_Exception/File_Not_Found_exception.java
+++ N) Exception Handling/Checked_Exception/File_Not_Found_exception.java
@@ -1,1 +1,11 @@
+import java.io.FileInputStream;
+public class File_Not_Found_Exception{
+    public static void main(String[] args) {
+        try{
+            FileInputStream fis=new FileInputStream("d:/abc.txt");
+        }catch(Exception e){
+            System.out.println(e);
+        }
 
+    }
+}
